70e19a88415854daef79b1602a90f06230180be5
Pegah Kassraian Fard <Pegahkf@gmail.com>
2019-04-08 21:04:28 -0700

    Add files via upload

--- compute.m
+++ compute.m
@@ -1,9 +1,16 @@
 clear;
 close all;
 mfile_name          = mfilename('fullpath');
-[pathstr,name,ext]  = fileparts(mfile_name);
-cd(pathstr);
-mkdir('out');
-cd 'compute';
-mainFull;
+[pathstr,name,ext]  = fileparts(mfile_name); % take the address of the folder containing this script into 'pathstr' as string.
+                                             % It will be called from now
+                                             % on as '<main>'. It can be, 
+                                             % for example,
+                                             % 'C:\Users\Someone\Somefolder'
+                                             % if this file is adressed as
+                                             % 'C:\Users\Someone\Somefolder\compute.m'
+                                             
+cd(pathstr); % change current folder to the <main> (containing folder)
+mkdir('out'); % create new folder named out (in <main>) (If it already exists, it is kept untouched)
+cd 'compute'; %change current folder to <main>/compute (the one in the previous current folder)
+mainFull; % call the mainFull.m script in the current folder (i.e., <main>/compute)
 cd '..';
