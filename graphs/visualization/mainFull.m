03db66d439b3d596184d107b3fbb1ad5c9616514
Pegah Kassraian Fard <Pegahkf@gmail.com>
2019-04-08 21:14:19 -0700

    Add files via upload

--- graphs/visualization/mainFull.m
+++ graphs/visualization/mainFull.m
@@ -1,10 +1,10 @@
 noPlots = no_plots_given;
 noPlotsRoot = ceil(sqrt(noPlots));
-addpath(genpath('../common/'));
-addpath(genpath('../matfiles/'));
+addpath(genpath('../common/'));     %<main>/graphs/common
+addpath(genpath('../matfiles/'));   %<main>/graphs/mathfiles
 
-load('settings.mat');
-load('gaResultFull.mat');
+load('settings.mat');       % <main>/graphs/matfiles/settings.mat with name 'settings' (struct)
+load('gaResultFull.mat');   % <main>/graphs/matfiles/gaResultFull.mat with name 'results_full' (struct)
 % 
 % settings.gen_distr_short = settings.gen_distr;
 % settings.res_distr_short = settings.res_distr;
@@ -34,17 +34,19 @@ divFun = {'@(x,y) shannonjensen(x,y)'};
 for i = 1:length(divId)    
     if (divFilter(i))
 %        full_result = results_full.(divId{i}){1};
-        full_result = results_full.(divId{i});
+        full_result = results_full.(divId{i}); %results_full came from gaResultFull.mat load
 
         [scores, indices] = sort(full_result.scores,'ascend');
-        population = full_result.population(indices,:);
+        population = full_result.population(indices,:); %Sort parameters according to their scores. (Smaller is better)
 
-
+        %plotNSets function : <main>/graphs/visualization/plotNSets.m
+        %plotNSets generates florets with given optimized parameters (best 'noPlots' of them) (taken
+        %from settings.mat), and plots histograms of real and generated
+        %segment lengths
         [plot_data_gen_visualisation{resSetList_ordered}, plot_data_real_visualisation{resSetList_ordered},gen_data]=plotNSets(settings,population(popFilter{i},:),noPlotsRoot,noPlotsRoot,divName{i},eval(divFun{i}));
-        for soln=1:noPlots
-            gen_distributions.(['Solution_' num2str(soln)]).SegmentLengths=sort(gen_data{soln}(:,2));
-            gen_distributions.(['Solution_' num2str(soln)]).Gen_tree_vis=gen_data{soln};
+        for soln=1:noPlots %Generated data is saved.
+            gen_distributions.(['Solution_' num2str(soln)]).SegmentLengths=sort(gen_data{soln}(:,2)); %Only segment lengths (sorted)
+            gen_distributions.(['Solution_' num2str(soln)]).Gen_tree_vis=gen_data{soln}; % for dendrogram visualisation (level-segmentlength-floretID)
         end
     end    
 end
-
