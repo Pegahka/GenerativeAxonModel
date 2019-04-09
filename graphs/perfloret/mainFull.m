ed6a4f1fee2afc76ba4fa0eac0037dfdfdccaee9
Pegah Kassraian Fard <Pegahkf@gmail.com>
2019-04-08 21:13:32 -0700

    Add files via upload

--- graphs/perfloret/mainFull.m
+++ graphs/perfloret/mainFull.m
@@ -1,10 +1,10 @@
 noPlots = no_plots_given;
 
-addpath(genpath('../common/'));
-addpath(genpath('../matfiles/'));
+addpath(genpath('../common/'));     %<main>/graphs/common
+addpath(genpath('../matfiles/'));   %<main>/graphs/matfiles
 
-load('settings.mat');
-load('gaResultFull.mat');
+load('settings.mat');       % <main>/graphs/matfiles/settings.mat with name 'settings' (struct)
+load('gaResultFull.mat');   % <main>/graphs/matfiles/gaResultFull.mat with name 'results_full' (struct)
 
 %% visualization
 
@@ -18,33 +18,33 @@ divFun = {'@(x,y) shannonjensen(x,y)'};
 
 for i = 1:length(divId)
     foldername = divName{i};
-    mkdir(foldername);
+    mkdir(foldername); % create a folder <main>/graphs/perfloret/JS (It was deleted in graphs.m before calling this script)
             
 %    full_result = results_full.(divId{i}){1};
-    full_result = results_full.(divId{i});
+    full_result = results_full.(divId{i}); %results_full came from gaResultFull.mat load
     
     [scores, indices] = sort(full_result.scores,'ascend');
-    population = full_result.population(indices,:);            
+    population = full_result.population(indices,:); %Sort parameters according to their scores. (Smaller is better)            
     
-    gp.version = settings.generator_version;                            
+    gp.version = settings.generator_version;
     gp.no_florets = 426;
 
     %% params
-    real_data = extractFeaturesSegments('XY_floret_dendrogram.xml');
-    real_data = get_data(real_data); 
+    real_data = extractFeaturesSegments('XY_floret_dendrogram.xml'); %Real florets are outputted (level-segmentlength-floretID)
+    real_data = get_data(real_data); % Get the statistics of real florets (mean, variance etc)
     real_data_perfloret = real_data;
-    gaResult = population(1:noPlots,:);
+    gaResult = population(1:noPlots,:); % Take the best <noPlots> optimized parameters
 
     for param_ind = 1:size(gaResult,1)
         for i = 1:settings.no_params 
-            gp.(settings.params{i,1}) = gaResult(param_ind,i);
+            gp.(settings.params{i,1}) = gaResult(param_ind,i); %parameters are passed to 'gp' struct with their names as field name. gp.ParameterName=ParameterValue
         end
 
         titletext = 'Floretwise statistics for the floret generator with';
 
         gp.res_distr = settings.res_distr;
         gp.gen_distr = settings.gen_distr;
-        if gp.retract_p1 > 0
+        if gp.retract_p1 > 0 %Title details are being set
             titletext = [titletext ' retraction '];            
         else
             titletext = [titletext 'out retraction '];            
@@ -54,15 +54,18 @@ for i = 1:length(divId)
             titletext = [titletext 'and PL' sprintf('(%2.2f)', gp.pl_growth)];
         end
         
-        gen_data_res = generateMultipleFlorets(gp);
-        asymmetry(gen_data_res(:,1));
+        gen_data_res = generateMultipleFlorets(gp); % <main>/graphs/common/generator/generateMultipleFlorets.m. 
+                                                    % 426 (set above) florets are generated and outputted (level-segmentlength-floretID)
+        asymmetry(gen_data_res(:,1)); 
         %distfitres{param_ind} = allfitdist(gen_data_res(:,2),'NLogL');
         %kstest((log(gen_data_res(:,2)) - mean(log(gen_data_res(:,2))))/std(log(gen_data_res(:,2))));                
 
-        gen_data = get_data(gen_data_res);
+        gen_data = get_data(gen_data_res); % Get the statistics of generated florets
         gen_data_perfloret{resSetList_ordered,param_ind}=gen_data;
-        gen_distributions.(['Solution_' num2str(param_ind)]).Statistics=[gen_data(:,9) gen_data(:,1:8)];
-        %% plots        
+        gen_distributions.(['Solution_' num2str(param_ind)]).Statistics=[gen_data(:,9) gen_data(:,1:8)]; % Statistics of generated florets are hold and will be saved in gen_distributions.mat later
+        %% plots       
+        % Each column of real_data and gen_data is one type of statistics.
+        % They are plotted and compared in subplots
         title = 'Std of segment length'; xlabeltext = 'std of segment length/floret';
         ax = plot_data(real_data(:,1),gen_data(:,1), title,4,4,1,xlabeltext);
         title = 'Mean of segment length'; xlabeltext = 'mean of segment length/floret'; 
@@ -84,16 +87,19 @@ for i = 1:length(divId)
         figure(1);
         hL = legend(ax,{'biological data','generated data'});
         set(hL,'Position',  [0.5 0.12 0.1 0.1] ,'Units', 'normalized');
+        % axes() and text() below are for a general title. There is no explicit
+        % method to set a general title in figures with subplots. title()
+        % command adds title to the current subplot.
         axes('Position',[0 0 1 1],'Xlim',[0 1],'Ylim',[0 1],'Box','off','Visible','off','Units',...
             'normalized', 'clipping' , 'off');
         text(0.5, 1,titletext,'HorizontalAlignment' ,'center','VerticalAlignment', 'top', 'FontWeight', 'bold', 'FontSize', 14 * 0.7, 'FontUnits', 'normalized');
         set(gcf, 'color', [1 1 1]);           
        
-        filename_rel_path = [foldername '/' sprintf(['floretwise' '_%d'],param_ind) '.pdf'];
+        filename_rel_path = [foldername '/' sprintf(['floretwise' '_%d'],param_ind) '.pdf']; 
         filename_rel_path_fig = [foldername '/' sprintf(['floretwise' '_%d'],param_ind) '.fig'];
-        export_fig(filename_rel_path,'-nofontswap');
-        export_fig(filename_rel_path_fig,'-nofontswap');
-        clf('reset');
+        export_fig(filename_rel_path,'-nofontswap'); % save the figure as <main>/graphs/perfloret/JS/floretwise_x.pdf. This command uses a 3rd party tool
+        export_fig(filename_rel_path_fig,'-nofontswap'); % save the figure as <main>/graphs/perfloret/JS/floretwise_x.fig. This command uses a 3rd party tool
+        clf('reset'); % Reset the figure and reuse it instead of closing and opening a new one. This is faster.
     end    
 end
 
