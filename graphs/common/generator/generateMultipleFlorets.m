1692677559540380dfa2a7feddcf391a58458174
Pegah Kassraian Fard <Pegahkf@gmail.com>
2019-04-08 21:15:39 -0700

    Add files via upload

--- graphs/common/generator/generateMultipleFlorets.m
+++ graphs/common/generator/generateMultipleFlorets.m
@@ -11,14 +11,14 @@ function data = generateMultipleFlorets(parameters)
     
     % data: 
     
-    % n x 2 matrix, containint no_florets number of concatenated florets
-    % 1st column: depth, 2nd column: length
+    % n x 3 matrix, containing no_florets number of concatenated florets
+    % 1st column: depth, 2nd column: length, 3rd colum: Floret ID
     
     data = [];
     
     gp = parameters;
    
-    % checking is 2 parameter distribution is used for resource instead of
+    % checking if 2 parameter distribution is used for resource instead of
     % fixed resource value.
     
     if isfield(parameters,'r_shape')
@@ -39,10 +39,7 @@ function data = generateMultipleFlorets(parameters)
         
         % generating florets
         
-        generated_data = generateFloret(gp);
-        generated_data = [generated_data, i * ones(size(generated_data,1),1)];
-        data = [data; generated_data];
+        generated_data = generateFloret(gp); %generate Floret generates one floret with depth and segment length information. 
+        generated_data = [generated_data, i * ones(size(generated_data,1),1)]; %Floret ID is added to these
+        data = [data; generated_data]; % and multiple florets are concatenated in 'data' matrix 
     end
-    
-    
-    
